2f2a49a9474deef102ca341275a851fee0abbb11
Zack Newman <zacknewman@ZackBook-Air-2.local>
2020-04-09 09:21:23 -0700

    Normalize values received to display size and set positions

--- receive.py
+++ receive.py
@@ -5,20 +5,27 @@ pyautogui.FAILSAFE = True
 
 size = pyautogui.size()
 
-UDP_IP = "127.0.0.1"
+# UDP_IP  = '255.255.255.255'
+# UDP_IP = '127.0.0.1'
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+sock.bind(('', UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     mouseStr = data.decode('utf-8')
-    mousePos = mouseStr.split(" ")
+    mousePos = int(float(mouseStr.split(' ')[1]) * size[0]), int(float(mouseStr.split(' ')[2]) * size[1])
     
-    print("X:", mousePos[0], "Y:", mousePos[1])    
+    print('X:', mousePos[0], 'Y:', mousePos[1])    
+    # print(mousePos[0] + mousePos[1])    
+
+    pyautogui.moveTo(mousePos[0], mousePos[1])
 
     # Exit condition
-    if mousePos[0] < 0.05 and mousePos[1] < 0.05:
+    if mousePos[0] < 10 and mousePos[1] < 10:
         break
