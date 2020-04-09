d1d536e9ee96704e6ef336a3790a0045515b6dce
Zack Newman <zacknewman@ZackBook-Air-2.local>
2020-04-09 11:41:22 -0700

    Only send network traffic every so many frames

--- send.py
+++ send.py
@@ -21,21 +21,21 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 i = 0
 while True:
-    if i % 10 == 0:
-        continue
+    if i % 2000000 == 0:
+        # print(i)
 
-    mousePos = pyautogui.position()[0] / size[0], pyautogui.position()[1] / size[1]
-    mouseStr = 'pos' + ' ' + str(mousePos[0]) + ' ' + str(mousePos[1])
-    data = mouseStr.encode('utf-8')
+        mousePos = pyautogui.position()[0] / size[0], pyautogui.position()[1] / size[1]
+        mouseStr = 'pos' + ' ' + str(mousePos[0]) + ' ' + str(mousePos[1])
+        data = mouseStr.encode('utf-8')
 
-    # if mousePos != prevMousePos:
-    sock.sendto(data, (UDP_IP, UDP_PORT))
-        # prevMousePos = mousePos
-    # print (mouseStr.split(" ")[1]
+        # if mousePos != prevMousePos:
+        sock.sendto(data, (UDP_IP, UDP_PORT))
+            # prevMousePos = mousePos
+        # print (mouseStr.split(" ")[1]
 
-    # # Exit condition
-    # if mousePos[0] < 0.05 and mousePos[1] < 0.05:
-    #     break
+        # # Exit condition
+        # if mousePos[0] < 0.05 and mousePos[1] < 0.05:
+        #     break
 
     i += 1
 
